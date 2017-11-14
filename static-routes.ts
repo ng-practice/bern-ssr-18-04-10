73c660451783561f6a2ee2b86fccc46042b31ae7
Gregor Woiwode <gregor.woiwode@sap.com>
2017-11-14 15:34:35 +0100

    :rocket: enable prerendering

---
+++ static-routes.ts
@@ -0,0 +1,4 @@
+export const staticRoutes = [
+    '/',
+    '/books'
+];
--- webpack.server.config.js
+++ webpack.server.config.js
@@ -2,12 +2,18 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: {  server: './server.ts' },
-  resolve: { extensions: ['.js', '.ts'] },
+  entry: {
+    // This is our Express server for Dynamic universal
+    server: './server.ts',
+    // This is an example of Static prerendering (generative)
+    prerender: './prerender.ts'
+  },
   target: 'node',
-  // this makes sure we include node_modules and other 3rd party libraries
-  externals: [/(node_modules|main\..*\.js)/],
+  resolve: { extensions: ['.ts', '.js'] },
+  // Make sure we include all node_modules etc
+  externals: [/(node_modules|main\..*\.js)/,],
   output: {
+    // Puts the output at the root of the dist folder
     path: path.join(__dirname, 'dist'),
     filename: '[name].js'
   },
@@ -17,14 +23,14 @@ module.exports = {
     ]
   },
   plugins: [
-    // Temporary Fix for issue: https://github.com/angular/angular/issues/11580
-    // for "WARNING Critical dependency: the request of a dependency is an expression"
     new webpack.ContextReplacementPlugin(
+      // fixes WARNING Critical dependency: the request of a dependency is an expression
       /(.+)?angular(\\|\/)core(.+)?/,
       path.join(__dirname, 'src'), // location of your src
       {} // a map of your routes
     ),
     new webpack.ContextReplacementPlugin(
+      // fixes WARNING Critical dependency: the request of a dependency is an expression
       /(.+)?express(\\|\/)(.+)?/,
       path.join(__dirname, 'src'),
       {}
